d6f192cca2bf55a4dc1513481bda5543e6b0e818
Kevin <kev.saintigny.schmidt@gmail.com>
2020-06-23 16:01:27 +0200

    country

--- src/App.js
+++ src/App.js
@@ -1,47 +1,60 @@
 import React from "react";
 import Loading from "./components/Loading";
 import Country from "./components/Country";
-//import axios from "axios"; if you would like to use it 💪🏻
+import axios from "axios";
+
+const URL_API = `https://restcountries.eu/rest/v2/name/germany`;
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      data: {},
       userInput: "",
       loading: true,
     };
-    this.handleChange = (e) => {
-      this.setState({
-        userInput: e.target.value.trim(),
-      });
-    };
   }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      userInput: e.target.value.trim(),
+    });
+  };
+
   componentDidMount() {
     setTimeout(() => {
       this.setState({
         loading: false,
       });
     }, 2000);
+
+    axios(URL_API).then((res) => {
+      const { ...props } = res.data;
+      this.setState({ data: { ...props } });
+    });
   }
 
-  // fetch result
-  // let { name, capital, topLevelDomain, timezones, languages } = item;
-  // this.setState({
-  //   name,
-  //   capital,
-  //   topLevelDomain,
-  //   timezones,
-  //   languages,
-  //   loading: false,
-  // });
-
   render() {
+    const { data } = this.state;
+
     if (this.state.loading) return <Loading />;
     return (
       <React.Fragment>
         <h1>Country App</h1>
-
-        <Country data="sendSomething" />
+        <form onChange={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.userInput}
+            onChange={this.handleChange}
+            placeholder="Enter a county"
+          />
+          <button type="submit">Search</button>
+        </form>
+        <Country data={data} />
       </React.Fragment>
     );
   }
--- src/components/Country.js
+++ src/components/Country.js
@@ -4,6 +4,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    console.log(props.data);
   }
 
   render() {
